0c08da2f95fe73829acf4b34d4febf1febb6873d
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2021-10-10 14:45:29 +0200

    Microsoft.EntityFrameworkCore nuget packages added to .Data layer

--- DAKLXU_HFT_2021221.Client/Program.cs
+++ DAKLXU_HFT_2021221.Client/Program.cs
@@ -6,7 +6,8 @@ namespace DAKLXU_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            
+            //connection string
+            //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\CarDb.mdf;Integrated Security=True
         }
     }
 }
