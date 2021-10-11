cbb2dcbc1b4004d0c39dd44b7bfe0abc1890eedc
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2021-10-11 10:31:08 +0200

    XYZDbContext constructor and OnConfiguring

--- DAKLXU_HFT_2021221.Client/Program.cs
+++ DAKLXU_HFT_2021221.Client/Program.cs
@@ -6,8 +6,7 @@ namespace DAKLXU_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            //connection string
-            //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\CarDb.mdf;Integrated Security=True
+            
         }
     }
 }
---
+++ DAKLXU_HFT_2021221.Data/XYZDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAKLXU_HFT_2021221.Data
+{
+    public class XYZDbContext : DbContext 
+    {
+        //Constructor
+        public XYZDbContext(){
+            this.Database.EnsureCreated();
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\CarDb.mdf;Integrated Security=True");
+            }
+        }
+    }
+}
