e496404c7957535319c2d7b14f00ba26fd60a683
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2021-11-16 14:23:15 +0100

    BrandController (missing Put) & 1/2 RentACarController

---
+++ DAKLXU_HFT_2021221.Endpoint/Controllers/BrandController.cs
@@ -0,0 +1,58 @@
+﻿using DAKLXU_HFT_2021221.Logic;
+using DAKLXU_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DAKLXU_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        IBrandLogic bl;
+        public BrandController(IBrandLogic bl)
+        {
+            this.bl = bl;
+        }
+
+        // GET: /brand
+        [HttpGet]
+        public List<Brand> Get()
+        {
+            return bl.GetAll(); ;
+        }
+
+        // GET /brand/5
+        [HttpGet("{id}")]
+        public Brand Get(int id)
+        {
+            return bl.GetOne(id);
+        }
+
+        // POST /brand
+        [HttpPost]
+        public void Post([FromBody] Brand value)
+        {
+            bl.Insert(value);
+        }
+
+        // PUT api/<BrandController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Brand value)
+        {
+            //UPDATE RETHINK REQUIRED
+        }
+
+        // DELETE /brand/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            bl.Remove(bl.GetOne(id));
+        }
+    }
+}
---
+++ DAKLXU_HFT_2021221.Endpoint/Controllers/RentACarController.cs
@@ -0,0 +1,55 @@
+﻿using DAKLXU_HFT_2021221.Logic;
+using DAKLXU_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DAKLXU_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RentACarController : ControllerBase
+    {
+        IRentACarLogic rl;
+        public RentACarController(IRentACarLogic rl)
+        {
+            this.rl = rl;
+        }
+
+        // GET: /rentacar
+        [HttpGet]
+        public List<RentACar> Get()
+        {
+            return rl.GetAll();
+        }
+
+        // GET /rentacar/5
+        [HttpGet("{id}")]
+        public RentACar Get(int id)
+        {
+            return rl.GetOne(id);
+        }
+
+        // POST /rentacar
+        [HttpPost]
+        public void Post([FromBody] RentACar value)
+        {
+        }
+
+        // PUT api/<RentACarController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RentACarController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
