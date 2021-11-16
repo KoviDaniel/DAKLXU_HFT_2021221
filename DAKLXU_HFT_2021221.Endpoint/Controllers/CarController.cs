36b5fd6c13de42410f4db5817e1646f258789f99
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2021-11-16 14:14:50 +0100

    CarController created (Put part not ready)

---
+++ DAKLXU_HFT_2021221.Endpoint/Controllers/CarController.cs
@@ -0,0 +1,58 @@
+﻿using DAKLXU_HFT_2021221.Logic;
+using DAKLXU_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DAKLXU_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        ICarLogic cl;
+        public CarController(ICarLogic cl)
+        {
+            this.cl = cl;
+        }
+
+        // GET: /car
+        [HttpGet]
+        public List<Car> Get()
+        {
+            return cl.GetAll();
+        }
+
+        // GET /car/5
+        [HttpGet("{id}")]
+        public Car Get(int id)
+        {
+            return cl.GetOne(id);
+        }
+
+        // POST /car
+        [HttpPost]
+        public void Post([FromBody] Car value)
+        {
+            cl.Insert(value);
+        }
+
+        // PUT api/<CarController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Car value)
+        {
+            // UPDATE METÓDUS ÚJRAGONDOLÁS
+        }
+
+        // DELETE /car/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            cl.Remove(cl.GetOne(id));
+        }
+    }
+}
