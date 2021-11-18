29d8df547a05b09052aedea8f3c7acacfc5957a9
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2021-11-18 15:36:22 +0100

    NON-CRUD controller created

---
+++ DAKLXU_HFT_2021221.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,59 @@
+﻿using DAKLXU_HFT_2021221.Logic;
+using DAKLXU_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DAKLXU_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        IBrandLogic bl;
+        IRentACarLogic rl;
+        public StatController(IBrandLogic bl, IRentACarLogic rl)
+        {
+            this.bl = bl;
+            this.rl = rl;
+        }
+        // GET: /stat/carorderbyprice/5
+        [HttpGet("{id}")]
+        public IEnumerable<Car> CarOrderByPrice(int id)
+        {
+            return bl.CarOrderByPrice(id);
+        }
+
+        // GET: /stat/carsorderbyKM/5
+        [HttpGet("{id}")]
+        public IEnumerable<Car> CarsOrderbyKM(int id)
+        {
+            return bl.CarsOrderbyKM(id);
+        }
+
+        // GET: /stat/mostvaluablecarowner/5
+        [HttpGet("{id}")]
+        public IEnumerable<RentACar> MostValuableCarOwner(int id)
+        {
+            return bl.MostValuableCarOwner(id);
+        }
+
+        // GET: /stat/mostrunnedkm/5
+        [HttpGet("{id}")]
+        public IEnumerable<Car> MostRunnedKM(int id)
+        {
+            return rl.MostRunnedKM(id);
+        }
+
+        // GET: /stat/groupbymodels/5
+        [HttpGet("{id}")]
+        public IEnumerable<KeyValuePair<string, double>> GroupByModels(int id)
+        {
+            return rl.GroupByModels(id);
+        }
+    }
+}
