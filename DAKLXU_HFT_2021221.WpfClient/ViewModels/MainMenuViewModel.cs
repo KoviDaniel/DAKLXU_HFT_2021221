65f72d3b143c3490cdcad4fd5f3f94aab690f7ba
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-03-08 07:54:21 +0100

    MainMenuViewModel finished?

--- DAKLXU_HFT_2021221.WpfClient/ViewModels/MainMenuViewModel.cs
+++ DAKLXU_HFT_2021221.WpfClient/ViewModels/MainMenuViewModel.cs
@@ -1,13 +1,53 @@
-﻿using System;
+﻿using DAKLXU_HFT_2021221.WpfClient.Services;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace DAKLXU_HFT_2021221.WpfClient.ViewModels
 {
     public class MainMenuViewModel
     {
+        IBrandMenuService brandMenuService;
+        ICarMenuService carMenuService;
+        IRentACarMenuService rentACarMenuService;
 
+        public ICommand BrandMenuOpenerCommand { get; set; }
+        public ICommand CarMenuOpenerCommand { get; set; }
+        public ICommand RentACarMenuOpenerCommand { get; set; }
+
+        static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+        public MainMenuViewModel(IBrandMenuService bService, ICarMenuService cService, IRentACarMenuService rService)
+        {
+            if (!IsInDesignMode)
+            {
+                this.brandMenuService = bService;
+                this.carMenuService = cService;
+                this.rentACarMenuService = rService;
+
+                BrandMenuOpenerCommand = new RelayCommand(
+                    () => brandMenuService.OpenBrandMenuWindow()
+                    );
+                CarMenuOpenerCommand = new RelayCommand(
+                    () => carMenuService.OpenCarMenuWindow()
+                    );
+                RentACarMenuOpenerCommand = new RelayCommand(
+                    () => rentACarMenuService.OpenRentACarMenuWindow()
+                    );
+            }
+        }
     }
 }
