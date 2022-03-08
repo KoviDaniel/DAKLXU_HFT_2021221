bf43aa643d5c3360f28d9af27670ee6a677eb9b5
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-03-08 07:08:37 +0100

    Models reference added and MainWindowViewModel.cs created

---
+++ DAKLXU_HFT_2021221.WpfClient/ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAKLXU_HFT_2021221.WpfClient.ViewModels
+{
+    public class MainMenuViewModel
+    {
+
+    }
+}
