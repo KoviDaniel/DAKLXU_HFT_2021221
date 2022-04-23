9b9296df6249d8e1192fe0102bca18e03b08650c
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2022-04-23 10:00:40 +0200

    GET ALL works

--- DAKLXU_HFT_2021221.JSClient/wwwroot/script.js
+++ DAKLXU_HFT_2021221.JSClient/wwwroot/script.js
@@ -1,3 +1,19 @@
-﻿fetch('http://localhost:17167/rentacar')
+﻿let companies = [];
+fetch('http://localhost:17167/rentacar')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        companies = y;
+        console.log(companies);
+        display();
+    });
+
+function display()
+{
+    companies.forEach(t =>
+    {
+        document.getElementById('resultareas').innerHTML +=
+            "<tr><td>" + t.rentCarID + "</td><td>"
+            +t.rentName+"</td><td>"+t.rating+"</td></tr>"
+        console.log(t.rentName);
+    });
+}
